7eacffb0f2e43b4cbd9fb13a7b473b7084596f60
Felix05-08 <davidlucasdesousafelix09@gmail.com>
2026-09-21 23:52:58 -0300

    Refactor print statements and improve formatting

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,36 +3,37 @@ public class Runner {
     public static void main(final String[] args) {
 
         Conta minhaConta = new Conta(1001, 2000);
-        System.out.println(minhaConta); //Conta{numero=1001, saldo=2000.0, limite=100.0}
+        System.out.println(minhaConta);
 
         Conta destino = new Conta(10, 0);
 
         minhaConta.sacar(200);
-        System.out.println(minhaConta); //Conta{numero=1001, saldo=1800.0, limite=100.0}
+        System.out.println(minhaConta);
 
-        if(!minhaConta.sacar(10000)){
-            System.out.println("Saldo insuficiente"); //Saldo insuficiente
+        if (!minhaConta.sacar(10000)) {
+            System.out.println("Saldo insuficiente");
         }
 
         minhaConta.depositar(500);
-        System.out.println(minhaConta); //Conta{numero=1001, saldo=2300.0, limite=100.0}
+        System.out.println(minhaConta);
 
         minhaConta.transferir(destino, 400);
-        System.out.println(minhaConta); //Conta{numero=1001, saldo=1900.0, limite=100.0}
+        System.out.println(minhaConta);
 
-        if(!minhaConta.transferir(destino, 4000000)) {
-            System.out.println("Saldo insuficiente"); //Saldo insuficiente
+        if (!minhaConta.transferir(destino, 4000000)) {
+            System.out.println("Saldo insuficiente");
         }
 
         minhaConta.sacar(1950);
-        System.out.println(minhaConta); //Conta{numero=1001, saldo=0.0, limite=50.0}
+        System.out.println(minhaConta);
 
         minhaConta.depositar(50);
-        System.out.println(minhaConta); //Conta{numero=1001, saldo=0.0, limite=100.0}
+        System.out.println(minhaConta);
 
         double[] extrato = minhaConta.verExtrato();
-        for(int i = 0; i < extrato.length; i++) {
-            System.out.println(extrato[i]); // -200.0 500.0 -400.0 -1950.0 50.0
+
+        for (int i = 0; i < extrato.length; i++) {
+            System.out.println(extrato[i]);
         }
     }
 }
